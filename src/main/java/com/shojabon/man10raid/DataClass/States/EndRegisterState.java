e2d8ad45a3e8aef9c7d08cfd84718d0ae02ed9c6
sho <shojabon@gmail.com>
2021-09-11 18:10:26 +0900

    added STimer

--- src/main/java/com/shojabon/man10raid/DataClass/States/EndRegisterState.java
+++ src/main/java/com/shojabon/man10raid/DataClass/States/EndRegisterState.java
@@ -4,17 +4,19 @@ import com.shojabon.man10raid.DataClass.RaidGame;
 import com.shojabon.man10raid.DataClass.RaidPlayer;
 import com.shojabon.man10raid.DataClass.RaidStateData;
 import com.shojabon.man10raid.Man10Raid;
+import com.shojabon.man10raid.Utils.STimer;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 public class EndRegisterState extends RaidStateData {
 
     RaidGame raid = Man10Raid.api.currentGame;
+    STimer timerTillBegin = new STimer();
 
 
     @Override
     public void start() {
-        raid.currentGameTime = 0;
+        defineTimer();
         raid.dividePlayers();
         for(RaidPlayer player: raid.players.values()){
             if(player.getPlayer() == null) continue; //if offline
@@ -27,11 +29,24 @@ public class EndRegisterState extends RaidStateData {
                 sendHighlightedMessage(player.getPlayer(), "§b§l残念ながら本日の試合は落選しました");
             }
         }
+
+        timerTillBegin.start();
     }
 
 
     @Override
     public void end() {
+    }
+
+
+    public void defineTimer(){
+        timerTillBegin.setRemainingTime(60);
+        timerTillBegin.addOnIntervalEvent(e -> {
+            Bukkit.getServer().broadcastMessage(e + " seconds remainng");
+        });
+        timerTillBegin.addOnEndEvent(() -> {
+            Bukkit.getServer().broadcastMessage("end!");
+        });
     }
 
     public void sendHighlightedMessage(Player p, String message){
---
+++ src/main/java/com/shojabon/man10raid/Utils/STimer.java
@@ -0,0 +1,68 @@
+package com.shojabon.man10raid.Utils;
+
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
+public class STimer {
+
+    public int remainingTime = 0;
+    boolean timerMoving = false;
+
+    ArrayList<Runnable> onEndEvents = new ArrayList<>();
+    ArrayList<Consumer<Integer>> onIntervalEvents = new ArrayList<>();
+
+    Thread timerThread = new Thread(() -> {
+        while(timerMoving){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            remainingTime -= 1;
+            if(remainingTime < 0) {
+                //timer end
+                for(Runnable event: onEndEvents){
+                    event.run();
+                }
+                timerMoving = false;
+            }
+
+            //end of interval events
+            for(Consumer<Integer> event: onIntervalEvents){
+                event.accept(remainingTime);
+            }
+
+        }
+    });
+
+    public void setRemainingTime(int time){
+        this.remainingTime = time;
+    }
+
+    public void addRemainingTime(int time){
+        remainingTime += time;
+    }
+
+    public void removeRemainingTIme(int time){
+        remainingTime -= time;
+    }
+
+    public void stop(){timerMoving = false;}
+
+    public void start(){
+        timerMoving = true;
+        if(!timerThread.isAlive()) timerThread.start();
+    }
+
+    public void addOnEndEvent(Runnable callback){
+        onEndEvents.add(callback);
+    }
+
+    public void addOnIntervalEvent(Consumer<Integer> callback){
+        onIntervalEvents.add(callback);
+    }
+
+
+
+}
